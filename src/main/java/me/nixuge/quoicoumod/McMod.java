fec8a9ffee03185b1c15ee8707057efe905f633d
Nixuge <mail@nixuge.me>
2023-06-29 22:24:42 +0200

    Version bump (1.0.0->1.0.1)

--- src/main/java/me/nixuge/quoicoumod/McMod.java
+++ src/main/java/me/nixuge/quoicoumod/McMod.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class McMod {
     public static final String MOD_ID = "quoicoumod";
     public static final String NAME = "QuoiCouMod";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
 
     @Getter
